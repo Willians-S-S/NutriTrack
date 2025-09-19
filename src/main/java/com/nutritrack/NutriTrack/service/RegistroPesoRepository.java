4508c675724992bf302423bfaf35f234a931f7c9
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-19 12:16:25 -0300

    feat: crud registro de peso

---
+++ src/main/java/com/nutritrack/NutriTrack/service/RegistroPesoRepository.java
@@ -0,0 +1,20 @@
+package com.nutritrack.repository;
+
+import com.nutritrack.entity.RegistroPeso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RegistroPesoRepository extends JpaRepository<RegistroPeso, UUID> {
+
+    List<RegistroPeso> findByUsuarioIdAndDataMedicaoBetween(UUID usuarioId, LocalDate start, LocalDate end);
+
+    Optional<RegistroPeso> findByUsuarioIdAndDataMedicao(UUID usuarioId, LocalDate dataMedicao);
+
+    List<RegistroPeso> findByUsuarioId(UUID usuarioId);
+}

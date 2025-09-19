35e71965ac2ca0933dc80471c347a7dd967b02c8
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-09-19 08:35:25 -0300

    feat: add entity usuario

---
+++ src/main/java/com/nutritrack/NutriTrack/entity/Usuario.java
@@ -0,0 +1,64 @@
+package com.nutritrack.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_usuario")
+    private UUID id;
+
+    @Column(nullable = false, length = 120)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha_hash", nullable = false)
+    private String senhaHash;
+
+    @Column(name = "altura_m", nullable = false, precision = 4, scale = 2)
+    private BigDecimal alturaM;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "nivel_atividade", nullable = false)
+    private NivelAtividade nivelAtividade;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "objetivo_usuario", nullable = false)
+    private ObjetivoUsuario objetivoUsuario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "criado_em", updatable = false)
+    private OffsetDateTime criadoEm;
+
+    @UpdateTimestamp
+    @Column(name = "atualizado_em")
+    private OffsetDateTime atualizadoEm;
+}

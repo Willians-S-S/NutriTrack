f6b07d1722eac229775f99ad5226bf43d0177e4c
Hectorsalg <hectorsalg@ufpi.edu.br>
2025-09-30 14:04:38 -0300

    feat: Nova aparência do botão

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,9 +1,20 @@
 import "./Button.scss";
-import type { MouseEventHandler } from "react";
+import type { MouseEventHandler, ReactNode } from "react";
 
-const Button = ({ title, onClick }: { title: string; onClick: MouseEventHandler<HTMLButtonElement> }) => {
+const Button = ({
+  title,
+  onClick,
+  white = false,
+}: {
+  title: ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  white?: boolean;
+}) => {
   return (
-    <button className="custom-button" onClick={onClick}>
+    <button
+      className={`custom-button ${white ? "custom-button--white" : ""}`}
+      onClick={onClick}
+    >
       {title}
     </button>
   );

284b021a5bdd9ec2ef1f89c9d92b4ed2633f33f7
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-10-20 21:35:58 -0300

    refactor query

--- src/main/java/com/nutritrack/NutriTrack/repository/RefeicaoRepository.java
+++ src/main/java/com/nutritrack/NutriTrack/repository/RefeicaoRepository.java
@@ -3,19 +3,21 @@ package com.nutritrack.NutriTrack.repository;
 import com.nutritrack.NutriTrack.entity.Refeicao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.OffsetDateTime;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RefeicaoRepository extends JpaRepository<Refeicao, UUID> {
 
-    List<Refeicao> findByUsuarioIdAndDataHoraBetween(UUID usuarioId, OffsetDateTime start, OffsetDateTime end);
+    @Query(value = "SELECT * FROM refeicoes r WHERE r.id_usuario = :usuarioId AND CAST(r.data_hora AS DATE) BETWEEN :startDate AND :endDate", nativeQuery = true)
+    List<Refeicao> findByUsuarioIdAndDateBetween(@Param("usuarioId") UUID usuarioId, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
     List<Refeicao> findByUsuarioId(UUID usuarioId);
 
-    @Query("SELECT r FROM Refeicao r WHERE r.usuario.id = :usuarioId AND date_trunc('day', r.dataHora) = date_trunc('day', :data)")
-    List<Refeicao> findByUsuarioIdAndDay(UUID usuarioId, OffsetDateTime data);
+    @Query(value = "SELECT * FROM refeicoes r WHERE r.id_usuario = :usuarioId AND CAST(r.data_hora AS DATE) = CAST(:data AS DATE)", nativeQuery = true)
+    List<Refeicao> findByUsuarioIdAndDay(@Param("usuarioId") UUID usuarioId, @Param("data") LocalDate data);
 }

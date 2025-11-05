5005321cc1991c3a97c11581a467578e2b57006b
Will <will@pop-os.local>
2025-11-05 11:40:45 -0300

    feat: autorizaçao

--- src/main/java/com/nutritrack/NutriTrack/controller/MetaController.java
+++ src/main/java/com/nutritrack/NutriTrack/controller/MetaController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("/api/v1/usuarios/{usuarioId}/metas")
+@PreAuthorize("hasAuthority('ROLE_ADMIN') or @authorization.isAuthorized(#id, authentication)")
 public class MetaController {
 
     @Autowired
